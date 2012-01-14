ed73fa0c98cbff071ba19cfa4be48c26be6bff41
Jeremy Lecerf <redpist@redpist.com>
2012-01-14 17:35:10 +0100

    simple main to launch ui before abstractions

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,15 @@
-int main(int, char **)
+#include <QMainWindow>
+
+#include "ui_MainWindow.h"
+
+int main(int argc, char *argv[])
 {
-	return 0;
+	  QApplication app(argc, argv);
+    QMainWindow mainWindow;
+
+		Ui::MainWindow			mainWindow_UI;
+
+		mainWindow_UI.setupUi(&mainWindow);
+    mainWindow.show();
+    return app.exec();
 }
