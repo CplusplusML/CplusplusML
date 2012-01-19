ddc1bf2adac1122344311b35dc7f4d94c42734d4
Jeremy Lecerf <redpist@redpist.com>
2012-01-19 22:45:24 +0100

    ui abstract start

---
+++ include/abstract/ui.h
@@ -0,0 +1,22 @@
+#ifndef _ABSTRACT_UI_H_
+#define _ABSTRACT_UI_H_
+
+template <template <class> class T>
+class UI_I
+{
+public:
+	UI_I(int argc, char *argv[])
+	{
+		
+	}
+
+	int		launch()
+	{
+		
+	}
+
+	~UI_I() { }
+};
+
+
+#endif /* _ABSTRACT_UI_H_ */
