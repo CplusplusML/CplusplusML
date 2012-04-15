34f31f69fbf5da2e25d5b7f482fb38c1f61ce4d7
Jeremy Lecerf <redpist@redpist.com>
2012-04-16 00:45:29 +0200

    patch

--- src/diagramitem.cpp
+++ src/diagramitem.cpp
@@ -71,7 +71,10 @@
                      << QPointF(100, -80) << QPointF(-100, -80)
                        << QPointF(-100, 100)
                        << QPointF(100, 100)
-                       << QPointF(100, -80);
+                       << QPointF(100, -80)
+                       << QPointF(100, 0)
+                       << QPointF(-100, 0)
+             ;
              break;
          default:
              myPolygon << QPointF(-120, -80) << QPointF(-70, 80)
