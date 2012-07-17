cb96c932b61598ac9c5f7998a9e7ad98d96ded14
Ugy <tony.tanon@gmail.com>
2012-07-17 13:55:15 +0200

    Adding object in DiagramScene (commented)

--- src/ui/diagram_scene.cc
+++ src/ui/diagram_scene.cc
@@ -7,8 +7,28 @@ namespace			CplusplusML
 {
   void				DiagramScene::mousePressEvent(QGraphicsSceneMouseEvent *mouseEvent)
   {
+    Object::Basic_		*item;
+
     if (mouseEvent->button() != Qt::LeftButton)
       return;
+
+    // switch (currentItem_)
+    //   {
+    //   case (objectClass):
+    // 	item = new Object::Class();
+    // 	break;
+    //   case (objectStruct):
+    // 	item = new Object::Struct();
+    // 	break;
+    //   case (objectUnion):
+    // 	item = new Object::Union();
+    // 	break;
+    //   default:
+    // 	break;
+    //   };
+    
+    // item->setPos(mouseEvent->scenePos());
+    // addItem(item);
 
     QGraphicsScene::mousePressEvent(mouseEvent);
   }
