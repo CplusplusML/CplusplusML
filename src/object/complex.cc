f78709e3c11898e369f66cc57ff4e870fbf197b9
Jeremy Lecerf <redpist@redpist.com>
2012-07-17 15:03:17 +0200

    centering middle

--- src/object/complex.cc
+++ src/object/complex.cc
@@ -10,24 +10,35 @@ void Object::Complex_::Render(void)
   QGraphicsProxyWidget *proxy = new QGraphicsProxyWidget();
   proxy->setWidget(titleLabel);
   titleLabel->setMinimumWidth(240);
+
   titleLabel->setFixedWidth(titleLabel->width() + 40);
+
+  int width = titleLabel->width();
+  int height = 125;
+
+  int x = proxy->x() - width / 2;
+  int y = proxy->y() - height / 2;
+
+  proxy->setX(x);
+  proxy->setY(y);
+
   QGraphicsPolygonItem *polygon;
   {
-    QRect titleBox(0, 0, titleLabel->width(), 25);
+    QRect titleBox(x, y, width, 25);
     polygon = new QGraphicsPolygonItem(static_cast<const QRectF &>(titleBox));
     polygon->setFillRule(Qt::OddEvenFill);
     polygon->setBrush(QColor(250, 250, 250));
   }
   QGraphicsPolygonItem *polygon2;
   {
-    QRect titleBox(0, 25, titleLabel->width(), 50);
+    QRect titleBox(x, y + 25, width, 50);
     polygon2 = new QGraphicsPolygonItem(static_cast<const QRectF &>(titleBox));
     polygon2->setFillRule(Qt::OddEvenFill);
     polygon2->setBrush(QColor(250, 250, 250));
   }
   QGraphicsPolygonItem *polygon3;
   {
-    QRect titleBox(0, 75, titleLabel->width(), 50);
+    QRect titleBox(x, y + 75, width, 50);
     polygon3 = new QGraphicsPolygonItem(static_cast<const QRectF &>(titleBox));
     polygon3->setFillRule(Qt::OddEvenFill);
     polygon3->setBrush(QColor(250, 250, 250));
