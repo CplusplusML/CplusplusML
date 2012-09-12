aef53114e79990c9140c2e93bd1428f067a4e36e
Pierre-Yves Le Borgne <pierre-yves.le-borgne@epitech.eu>
2012-09-12 17:01:45 +0200

    cleaned main and added comments for further dev

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,34 @@
 
 #include <iostream>
 
-#include <list>
-
 #include "assed.hpp"
 
 int main(void)
 {
   AST::AST ast;
 
+  // rvalue, on s'en branle, on fait un new avec,
+  // on l'assigne a un shared_ptr dans le std::vector
   ast << AST::Namespace("::");
+
+  return (0);
+
+  // on veut recuperer une reference au-dit Namespace
+  // cependant, si jamais ast est detruit, on veut
+  // quand meme avoir une reference namespc valide
+  // le coup d'utiliser des references, et bien ca pue
+  // du cul, surtout que derriere c'est gere par des
+  // shared_ptr et qu'on doit en profiter !
+  AST::Namespace &namespc = ast.Get(AST::Namespace("::"));
+}
+
+
+ /*
   ast.Get(AST::Namespace("::")) << AST::Class("class1");
   ast.Get(AST::Namespace("::")) << AST::Class("class2") << AST::Class("class42");
 
-  /*
+  std::cout << ast << std::endl;
+
   AST::Class &c = ast.Get(AST::Namespace("::")).Get(AST::Class("class1"));
 
   c << AST::Class("SubClass");
@@ -27,6 +42,3 @@ int main(void)
   // c << AST::Visibility::PRIVATE << AST::Array(ast.Get(AST::Namespace("::")).Get(AST::Class("class2")), "_tab", 42);
   // c.Get(AST::Array("_tab"));
   //  c << AST::Visibility::PUBLIC << AST::Function("function1");
-
-  std::cout << ast << std::endl;
-}
