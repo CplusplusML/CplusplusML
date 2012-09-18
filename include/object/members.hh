36d1740c43be9962bd060c3628e029845356c220
Ugy <tony.tanon@gmail.com>
2012-09-19 00:20:40 +0200

    indented members.hh

--- include/object/members.hh
+++ include/object/members.hh
@@ -1,104 +1,104 @@
-#ifndef		_CPLUSPLUSML_MEMBERS_HH_
-# define	_CPLUSPLUSML_MEMBERS_HH_
+#ifndef         _CPLUSPLUSML_MEMBERS_HH_
+# define        _CPLUSPLUSML_MEMBERS_HH_
 
-# include	<list>
-# include	<string>
-# include	<map>
+# include       <list>
+# include       <string>
+# include       <map>
 
-# include	<QGraphicsSimpleTextItem>
+# include       <QGraphicsSimpleTextItem>
 
 
-class Complex_;
+class                   Complex_;
 
-namespace			Object
+namespace               Object
 {
-  namespace			Members
+  namespace             Members
   {
     // Visibility utilities
-    enum			Visibility
+    enum                Visibility
       {
-	PUBLIC = 0,
-	PROTECTED = 1,
-	PRIVATE = 2,
-	UNKNOWN
+        PUBLIC = 0,
+        PROTECTED = 1,
+        PRIVATE = 2,
+        UNKNOWN
       };
 
-    inline const char		*visibilityName(Visibility v)
+    inline const char   *visibilityName(Visibility v)
     {
       static std::map<Visibility, char const *>	visibilities = {
-	{PUBLIC, "+"},
-	{PROTECTED, "#"},
-	{PRIVATE, "-"}
+        {PUBLIC, "+"},
+        {PROTECTED, "#"},
+        {PRIVATE, "-"}
       };
 
       return (visibilities[v]);
     };
 
     // AMember
-    struct			AMember
+    struct              AMember
     {
       AMember(bool tmpMember);
       virtual ~AMember();
 
-      void			operator=(AMember const &m);
-      inline void		updateLabel(void)
+      void              operator=(AMember const &m);
+      inline void       updateLabel(void)
       {
-	if (label)
-	  label->setText(this->toString().c_str());
+        if (label)
+          label->setText(this->toString().c_str());
       }
 
       virtual std::string	toString(void) const = 0;
 
-      std::string		name;
-      std::string		type;
-      Visibility		visibility;
-      bool			isStatic;
+      std::string       name;
+      std::string       type;
+      Visibility        visibility;
+      bool              isStatic;
 
       // Qt
-      QGraphicsSimpleTextItem	*label;
-      bool			deleted;
+      QGraphicsSimpleTextItem   *label;
+      bool              deleted;
     };
 
     // Attribute
-    struct			Attribute: public AMember
+    struct              Attribute: public AMember
     {
       Attribute(bool tmpMember = false);
 
-      void			operator=(Attribute const &a);
-      std::string		toString(void) const;
+      void              operator=(Attribute const &a);
+      std::string       toString(void) const;
 
-      std::string		defaultValue;
+      std::string       defaultValue;
     };
 
     // Operation
-    struct			Operation: public AMember
+    struct              Operation: public AMember
     {
-      enum			InheritanceType
-	{
-	  LEAF,
-	  VIRTUAL,
-	  PURE
-	};
+      enum              InheritanceType
+        {
+          LEAF,
+          VIRTUAL,
+          PURE
+        };
 
       Operation(bool tmpMember = false);
 
-      void			operator=(Operation const &o);
-      std::string		toString(void) const;
+      void              operator=(Operation const &o);
+      std::string       toString(void) const;
 
-      bool			isConst;
-      InheritanceType		inhType;
+      bool              isConst;
+      InheritanceType   inhType;
 
       // Structure for parameters
-      struct			Parameter
+      struct            Parameter
       {
-	std::string		toString(bool inList = false) const;
+        std::string     toString(bool inList = false) const;
 
-	std::string		name;
-	std::string		type;
-	std::string		defValue;
+        std::string     name;
+        std::string     type;
+        std::string     defValue;
       };
 	
-      std::list<Parameter>	parameters;
+      std::list<Parameter>      parameters;
     };
   }
 }
