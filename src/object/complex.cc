c5793f80635addc6f1c38ced2f71b902f26277ac
Ugy <tony.tanon@gmail.com>
2012-09-20 01:53:01 +0200

    adjust connection qui manquait

--- src/object/complex.cc
+++ src/object/complex.cc
@@ -39,6 +39,10 @@ namespace               Object
     addToGroup(attrRect_);
     addToGroup(opeRect_);
     setFlag(QGraphicsItem::ItemIsMovable, true);
+
+    for (int i = 0; i < 8; ++i)
+      connections.append(new ArrowConnection(this));
+    adjustConnections();
   }
 
   Complex_::~Complex_()
