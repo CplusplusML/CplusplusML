a0505cdfb80287db7dd0b22602bd2406aa2e9d04
Jeremy Lecerf <redpist@redpist.com>
2012-09-19 19:00:58 +0200

    new website link clean

--- src/ui/main_w.cc
+++ src/ui/main_w.cc
@@ -6,7 +6,6 @@
 
 #include "ui/main_w.h"
 #include "ui_AboutWindow.h"
-#include "ui_WebSiteWindow.h"
 #include "ui_HelpWindow.h"
 
 #include "object/class.hh"
@@ -24,8 +23,6 @@ namespace CplusplusML
       about_window_ui.setupUi(&aboutWindow_);
       connect(actionAbout_C_Ml, SIGNAL(triggered()), this, SLOT(About()));
 
-      Ui::WebSiteWindow	web_site_window_ui;
-      web_site_window_ui.setupUi(&webSiteWindow_);
       connect(actionC_Website, SIGNAL(triggered()), this, SLOT(WebSite()));
 
       Ui::HelpWindow	help_window_ui;
